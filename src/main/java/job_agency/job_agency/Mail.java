9351ec2429e55c0e35a815182be54758ba40a49e
kevin <e1025797@student.tuwien.ac.at>
2014-05-17 14:42:32 +0200

    snippets reinkopiert und rumgespielt

---
+++ src/main/java/job_agency/job_agency/Mail.java
@@ -0,0 +1,11 @@
+package job_agency.job_agency;
+
+public class Mail {
+
+//http://camel.apache.org/mail.html
+	
+//smtp://host[:port]?password=somepwd&username=someuser
+//smtp://mycompany.mailserver:30?password=tiger&username=scott
+	
+	
+}
